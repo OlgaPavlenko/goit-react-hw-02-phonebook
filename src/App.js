d63c2fda719af1391bd34e1d9747a2d1f0571d9f
Olga Pavlenko <olgaigpavlenko@gmail.com>
2020-12-24 01:00:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Form from './Components/Form/Form';
+import ContactList from './Components/ContactList/ContactList';
+import Filter from './Components/Filter/Filter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = contact => {
+    const isFinded = this.state.contacts.find(
+      ({ name }) =>
+        name.toLocaleLowerCase() === contact.name.toLocaleLowerCase(),
+    );
+
+    if (isFinded) {
+      alert(`${contact.name} is already in the contacts`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
+  onFilterChanged = filter => {
+    this.setState({ filter });
+  };
+
+  filterContactList = () => {
+    let { contacts, filter } = this.state;
+    filter = filter.toLocaleLowerCase();
+
+    return contacts.filter(({ name }) =>
+      name.toLocaleLowerCase().includes(filter),
+    );
+  };
+
+  onDelete = rId => {
+    let contacts = this.state.contacts.filter(
+      ({ id }) => id.toLocaleLowerCase() !== rId,
+    );
+
+    this.setState({ contacts });
+  };
+
+  render() {
+    const filteredContactList = this.filterContactList();
+
+    return (
+      <>
+        <Form onSubmit={this.addContact} />
+        <Filter onFilterChanged={this.onFilterChanged} />
+        <ContactList
+          contactsList={filteredContactList}
+          onDelete={this.onDelete}
+        />
+      </>
+    );
+  }
 }
-
-export default App;
