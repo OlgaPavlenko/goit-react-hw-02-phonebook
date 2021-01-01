90d1b7835a41588f0748a5f4a4c3d2101d6d819c
Olga Pavlenko <olgaigpavlenko@gmail.com>
2021-01-01 19:22:43 +0200

    Created repository - goit-react-hw-03-phonebook

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
     ],
     filter: '',
   };
-
+  k;
   addContact = contact => {
     const isFinded = this.state.contacts.find(
       ({ name }) =>
@@ -50,6 +50,21 @@ export default class App extends Component {
 
     this.setState({ contacts });
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const filteredContactList = this.filterContactList();
